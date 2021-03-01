e23f5b1d421389860b4d2d3d6950acac61bc335d
Mikail Khan <mikail.khan45@gmail.com>
2021-03-01 10:12:36 -0500

    reverted

--- js/index.js
+++ js/index.js
@@ -1,22 +1,59 @@
 import {parse, HtmlGenerator} from 'latex.js'
+import 'brace'
+import "../css/site.css"
 
-import("../pkg/index.js").then(calculatex => {
-    let generator = new HtmlGenerator({hyphenate: false});
-    document.head.appendChild(generator.stylesAndScripts("https://cdn.jsdelivr.net/npm/latex.js@0.12.4/dist/"))
+window.onload = () => {
+    import("../pkg/index.js").then(calculatex => {
+        let generator = new HtmlGenerator({hyphenate: false});
+        document.head.appendChild(generator.stylesAndScripts("https://cdn.jsdelivr.net/npm/latex.js@0.12.4/dist/"))
 
-    window.render = (input) => {
-        generator.reset();
-        document.cookie = "last_input = " + escape(input) + ";" + "SameSite=Lax";
+        var editor = ace.edit("editor");
 
         try {
-            let latex = calculatex.run_calculatex(input);
-            let rendered = parse(latex, {generator: generator}).domFragment();
+            const cookieValue = document.cookie
+                .split('; ')
+                .find(row => row.startsWith('last_input'))
+                .split('=')[1];
+            if (cookieValue != undefined) {
+                editor.setValue(unescape(cookieValue), 0);
+                editor.clearSelection();
+            }
+        } catch {}
 
-            document.getElementById("error").innerHTML = "";
-            document.getElementById("output").innerHTML = "";
-            document.getElementById("output").appendChild(rendered);
-        } catch (e) {
-            document.getElementById("error").innerHTML = e;
+        // very strange things happen when it renders
+        // directly off of editor.on('change')
+        var changed = true;
+
+        let render_textbox = () => {
+            const e = editor.getValue();
+            render(e + "\n");
         }
-    };
-}).catch(console.error);
+
+        setTimeout(() => setInterval(() => {
+            if (changed) {
+                render_textbox();
+                changed = false;
+            }
+        }, 300), 500);
+
+        editor.on('change', () => {
+            changed = true;
+        });
+
+        window.render = (input) => {
+            generator.reset();
+            document.cookie = "last_input = " + escape(input) + ";" + "SameSite=Lax";
+
+            try {
+                let latex = calculatex.run_calculatex(input);
+                let rendered = parse(latex, {generator: generator}).domFragment();
+
+                document.getElementById("error").innerHTML = "";
+                document.getElementById("output").innerHTML = "";
+                document.getElementById("output").appendChild(rendered);
+            } catch (e) {
+                document.getElementById("error").innerHTML = e;
+            }
+        };
+    }).catch(console.error);
+}
